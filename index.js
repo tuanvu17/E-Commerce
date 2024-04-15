092f9709304a84f7c6e032f5694beba11df49aff
tuanvu17 <tuanvu17mta@gmail.com>
2024-04-15 15:32:19 +0700

    Blog - Category

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
+const categoryRouter = require('./routes/prodcategoryRoute');
+const blogcategoryRouter = require('./routes/blogCatRoute');
+
+
 
 const cookieParser = require("cookie-parser");
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const morgan = require("morgan");
+
 
 
 dbConnect();
@@ -23,7 +28,8 @@ app.use(cookieParser());
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
-
+app.use('/api/category', categoryRouter);
+app.use("/api/blogcategory", blogcategoryRouter);
 app.use(notFound);
 app.use(errorHandler);
 
